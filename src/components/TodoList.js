e60dd8d65078d9c20b067fda175506fc9a1ad8e9
sean-mahan <seanmahansean@gmail.com>
2021-06-02 00:40:59 -0500

    first

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+class TodoList extends React.Component{
+  constructor(){
+    super();
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h2>Welcome to your Todo App!</h2>
+      </div>
+    );
+  }
+}
